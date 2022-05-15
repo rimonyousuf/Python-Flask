66c92f7241aba5578c1556b7e6b9e9cb997d1c84
Rimon Yousuf <rimonyousuf00@gmail.com>
2022-05-15 14:23:25 +0600

    Static and Templates folder

--- Creating Our First Flask App.py
+++ Creating Our First Flask App.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
+
+@app.route("/about")
+def Rimon():
+    name='Rimon Yousuf'
+    return render_template('about.html', name2=name)
 
 app.run(debug=True)
